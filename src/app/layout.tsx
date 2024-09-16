ba5e99e99629827c866a2fabcca8ceef6fd011c0
matyas-toth <matyastoth.mail@gmail.com>
2024-09-16 09:50:00 +0200

    Added note to myself

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-
+// Aceternity UI
 
 export const metadata: Metadata = {
   title: "Reig Industries | Backend development & graphic design",
